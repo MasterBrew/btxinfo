8e053d344b37d6b2b6b6d92f0b1fc475e4b7aa53
Yes Sir its Me <mantronix@email.com>
2019-05-28 10:09:05 +0200

    Add files via upload

---
+++ btxinfo.py
@@ -0,0 +1,61 @@
+import json
+import requests
+from pprint import *
+
+header = {'Content-Type':  'application/json; charset=utf-8'}
+
+urlApi = 'http://bitcore.cc/api.php'
+
+
+response = requests.get(urlApi, headers=header)
+a = json.dumps(response.json())    # dump response to a as a string
+b = json.loads(a)              # decode json format b
+
+btx_data = b["data"]
+
+# Get info from second website...  http://whattomine.com
+urlApi = 'http://whattomine.com/coins.json'
+response = requests.get(urlApi, headers=header)
+a = json.dumps(response.json())    # dump response to a
+b = json.loads(a)              # decode json format b
+
+coins = b["coins"]
+bitcore = (coins["Bitcore"])
+netHash = bitcore['nethash'] / 1000000000
+
+# Bitcore BTX information from https://api.coinmarketcap.com/
+urlApi = 'https://api.coinmarketcap.com/v2/ticker/1654/'
+response = requests.get(urlApi, headers=header)
+
+# String
+a = json.dumps(response.json())    # dump response to a
+# Dict
+b = json.loads(a)              # decode json format b
+
+# print(type(a))
+# pprint(a)
+# print(type(b))
+# pprint(b['data'])
+# pprint(b.keys())
+
+coinCap = b['data']
+coinPrice = coinCap['quotes']['USD']
+algoDiff = bitcore['difficulty']
+algoDiff24 = bitcore['difficulty24']
+change_percent = ((float(algoDiff) - algoDiff24) / algoDiff24) * 100
+
+print("")
+print("*" * 28 + " Mantronix Rules! " + "*" * 28)
+print(f"            Name:  {coinCap['name']} [{coinCap['symbol']}]   -   Algorithm  : {bitcore['algorithm']}\n")
+print(f"         Market_cap: {bitcore['market_cap']}   Price[{coinCap['symbol']}] ${coinPrice['price']:3.2f}   Rank: {coinCap['rank']}")
+print(f"      Exchange rate: {round(bitcore['exchange_rate']*100000000)} satoshi   Exchange rate 24H: {round(bitcore['exchange_rate24']*100000000)} satoshi\n")
+print(f"             Block Time: {bitcore['block_time']} Sec. - Reward: {bitcore['block_reward']} BTX/Block")
+print(f"      Last Block: {bitcore['last_block']}  Timestamp: {bitcore['timestamp']} Left Supply: {float(btx_data['leftsupply']):3.2f}\n")
+print(f"           *  Current Difficulty: {algoDiff:3.2f}  NetHash: {netHash:3.2f} Gh/s  *\n")
+print(f"     Maximum: {coinCap['max_supply']}  Total: {coinCap['total_supply']}  Circulating: {coinCap['circulating_supply']}\n")
+print(f"     Airdrop: {float(btx_data['airdrop']):3.2f} Virtual Fork: {float(btx_data['vfork']):3.2f}  Claimed: {float(btx_data['claimed']):3.2f}")
+print(f"    Mining: {float(btx_data['mining']):3.2f}  Marketing: {float(btx_data['marketing']):3.2f}  Circulating: {float(btx_data['circulating']):3.2f}")
+print(f"    Average last 24H: {bitcore['difficulty24']:3.2f} - Last 24 hours  Change/24 Hour : {change_percent:3.2f}%")
+print("*" * 74)
+
+input('\n                       -+-  key to continue  --+--')
---
+++ py_to_.exe.bat
@@ -0,0 +1,24 @@
+@echo off
+cls
+
+echo 000 INFO: Adding Path to Microsoft.Windows.Common-Controls Location
+set PATH=%PATH%;C:\Windows\System32\downlevel;
+
+echo 000 INFO: Moving to the Magic place
+cd C:\Python37\scripts
+
+echo 000 INFO: Mantronix is shuffeling the box of O and 1's.
+
+pyinstaller --onefile --clean --distpath C:\Users\gebruiker\Desktop\btx^
+	    --icon=C:\Users\gebruiker\Desktop\btx\favicon.ico^
+	    --add-data=C:\Users\gebruiker\Desktop\btx\favicon.ico;img^
+ 	    C:\Users\gebruiker\Desktop\btx\btxinfo.py
+
+echo 99999 INFO: Moving to the Birthplace of this monster
+cd C:\Users\gebruiker\Desktop\btx
+
+echo 99999 INFO: Testing if its realy works! 
+btxinfo.exe
+echo 'Done..'
+pause
+
